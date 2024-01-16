a84edc6ce75c0ced1c8bf12ddb7d0e16461999c6
narumol01 <73689289+narumol01@users.noreply.github.com>
2024-01-16 17:17:35 +0700

    first commit

---
+++ src/main/kotlin/com/tungty/tungtyservice/TungtyServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.tungty.tungtyservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TungtyServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TungtyServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/tungty/tungtyservice/TungtyServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tungty.tungtyservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TungtyServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
