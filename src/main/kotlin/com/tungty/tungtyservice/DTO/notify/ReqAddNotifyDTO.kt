950629bbc371389449259428dfce42dff54d7707
narumol01 <73689289+narumol01@users.noreply.github.com>
2024-03-22 21:57:43 +0700

    change notify structure data and change logic job

--- src/main/kotlin/com/tungty/tungtyservice/DTO/notify/ReqAddNotifyDTO.kt
+++ src/main/kotlin/com/tungty/tungtyservice/DTO/notify/ReqAddNotifyDTO.kt
@@ -1,18 +1,6 @@
 package com.tungty.tungtyservice.DTO.notify
 
 data class ReqAddNotifyDTO(
-//    val id: String?,
-//    val type: String,
-////    val flagActive: Boolean,
-//    val partyId: String,
-//    val partyName: String,
-////    val createDate: String,
-//    val userId: String,
-//    val appointmentTime: String,
-//    val appointmentDate: String
-
-
-//    val id: String,
     val partyName: String,
     val typeAction: String,
     val notifyDescription: String,
@@ -21,5 +9,4 @@ data class ReqAddNotifyDTO(
     val appointmentTime: String,
     val status: String,
     val partyId: String
-
 )
--- src/main/kotlin/com/tungty/tungtyservice/job/NotifyJob.kt
+++ src/main/kotlin/com/tungty/tungtyservice/job/NotifyJob.kt
@@ -10,15 +10,12 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
-import java.awt.SystemTray
-import java.time.Duration
-import java.time.LocalDateTime
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
-import java.util.*
-import java.util.concurrent.ScheduledFuture
-import java.util.concurrent.atomic.AtomicInteger
 import java.text.SimpleDateFormat
+import java.util.*
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 
 @Service
@@ -26,76 +23,34 @@ class NotifyJob() {
 
     @Autowired
 lateinit var notifyRepository: NotifyRepository
-
     fun getCurrentTimeWithOffset(): String {
         val currentLocale = Locale.getDefault() // Get user's locale (optional)
         val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", currentLocale) // Adjust format if necessary
         dateFormat.timeZone = TimeZone.getTimeZone("Asia/Bangkok") // Set time zone offset (+07:00 for Bangkok)
         return dateFormat.format(Date())
     }
-    fun doGetData(element: NotifyEntity): Mono<NotifyEntity> {
-//        if(element.appointmentTime == getCurrentTimeWithOffset()){
-            System.out.println("In TIME!!!")
-            element.status = "Unread"
-//        }
-        notifyRepository.save(element)
-        Thread.sleep(1000);
-        return element.toMono()
+    fun setStatus(data: NotifyEntity): NotifyEntity{
+        data.status = "Unread"
+        return data
     }
     @Scheduled(cron = "0 * * ? * *")
-    fun addNewTask() {
+    fun checkTimeStartParty() {
         System.out.println("Check !");
         try {
             val datasnoti = notifyRepository.findAll()
-            val fildatsnoti = datasnoti.filter({ele -> ele.appointmentTime == "2024-03-13T03:22:00+07:00"}).collectList().block()!!
-            fildatsnoti.forEach(System.out::println)
-            for(noti in fildatsnoti){
-                noti.status = "Unread"
-                System.out.println("$noti !!")
-                notifyRepository.save(noti)
-                System.out.println("Do save TIME !!")
+            System.out.println(getCurrentTimeWithOffset())
+            val fildatsnoti = datasnoti.filter({ele -> ele.appointmentTime == getCurrentTimeWithOffset()}).map {
+                setStatus(it)
+            }.collectList().block()!!
+            if (fildatsnoti != null) {
+                notifyRepository.saveAll(fildatsnoti)
+                    .subscribe { result ->
+                        System.out.println(result)
+                    }
             }
-//            System.out.println()
 
         } catch (e: Exception) {
             System.out.println("Error: $e")
         }
-
-//        datanoti.doOnNext{ element ->
-//            if (element.appointmentTime == getCurrentTimeWithOffset()){
-//                System.out.println()
-//                System.out.println("IT TIME!! IT TIME!! NOW GO CHANGE STATUS ${element.partyName}")
-//                val chagestatus = NotifyEntity(
-//                    _id = element._id,
-//                    partyName = element.partyName,
-//                    typeAction = element.typeAction,
-//                    notifyDescription = element.notifyDescription,
-//                    userId = element.userId,
-//                    appointmentDate = element.appointmentDate,
-//                    appointmentTime = element.appointmentTime,
-//                    status = "Unread",
-//                    partyId = element.partyId
-//                )
-//                System.out.println(chagestatus)
-//                notifyRepository.save(
-//                    chagestatus
-//                )
-//            }
-//        }.subscribe()
-
-
-//            .subscribe { element ->
-//            System.out.println("Processing element: $element");
-//            if(element.status == "Pending"){
-//                System.out.println("THIS IS PENDING !!! BRO IT GONNA ALERT IN ${element.appointmentTime}")
-//            }
-//            System.out.println(getCurrentTimeWithOffset())
-//            if (element.appointmentTime == getCurrentTimeWithOffset()){
-//                System.out.println()
-//                System.out.println("IT TIME!! IT TIME!! NOW GO CHANGE STATUS ${element.partyName}")
-//                doAddData(element)
-//                System.out.println(notifyRepository.findAll())
-//            }
-
     }
 }
